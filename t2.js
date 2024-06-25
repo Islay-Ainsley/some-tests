262f56cbd760359d194852d1efed6430d967c4c5
Islay-Ainsley <Islay.Ainsley1@gmail.com>
2024-06-25 12:44:16 +0300

    Create t2.js

---
+++ t2.js
@@ -0,0 +1,5 @@
+/*eslint quotes: ["error", "double"]*/
+
+var single = 'single';
+var unescaped = 'a string containing "double" quotes';
+var backtick = `back\ntick`; // you can use \n in single or double quoted strings
